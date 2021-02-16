2ceebcd14f8a16322c610a5bebbfbdaf7f88b718
Zhanho Shing <zhanhoshing@icloud.com>
2021-02-16 13:56:06 +0800

    feat: added fields in BaseEntry

--- Settings.Net/SettingsEntry/BaseEntry.cs
+++ Settings.Net/SettingsEntry/BaseEntry.cs
@@ -1,5 +1,15 @@
 ﻿namespace Settings.Net.SettingsEntry {
-    public class BaseEntry {
-        
+    /// <summary>
+    /// A base class for settings entry. This class is abstract and shall not be used in your code.
+    /// </summary>
+    public abstract class BaseEntry {
+        /// <summary>
+        /// The ID of the entry. The ID is unique under an entry group.
+        /// </summary>
+        public abstract string ID { get; }
+        /// <summary>
+        /// A descriptive line of this entry. Description is optional.
+        /// </summary>
+        public abstract string Description { get; }
     }
 }
