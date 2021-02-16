9a181c032fb516f2a43069cde7f5edf716023f25
Zhanho Shing <zhanhoshing@icloud.com>
2021-02-16 15:17:29 +0800

    feat: added StringEntry class

---
+++ Settings.Net/SettingsEntry/StringEntry.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Settings.Net.SettingsEntry {
+    /// <summary>
+    /// Represents an entry with string value.
+    /// </summary>
+    public class StringEntry : BaseEntry {
+        /// <summary>
+        /// Initializes a StringEntry object from existing JSON data.
+        /// </summary>
+        /// <param name="jToken">The existing JToken</param>
+        internal StringEntry(JToken jToken) {
+            
+        }
+
+        /// <summary>
+        /// Initializes a new StringEntry object.
+        /// </summary>
+        /// <param name="ID">The ID of the new entry.</param>
+        /// <param name="value">The value of the new entry.</param>
+        internal StringEntry(string ID, string value) {
+            
+        }
+        
+        public override string ID { get; }
+        public override string Description { get; }
+        /// <summary>
+        /// The value of the entry
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
