0b182d514d7c33c59c403cd0acf7d77a5351ebbb
Zhanho Shing <zhanhoshing@icloud.com>
2021-02-17 20:24:45 +0800

    feat: add InvalidEntryValueException class

---
+++ Settings.Net/Exceptions/InvalidEntryValueException.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+using System;
+
+namespace Settings.Net.Exceptions {
+    /// <summary>
+    /// Throws if the type of value does not match the object's value type.
+    /// </summary>
+    [Serializable]
+    public class InvalidEntryValueException : EntryTypeNotMatchException {
+        public InvalidEntryValueException() { }
+
+        public InvalidEntryValueException(string? expectedType, string? receivedType) : base(expectedType, receivedType) { }
+
+        public InvalidEntryValueException(string? expectedType, string? receivedType, string? messages) : base(
+            expectedType, receivedType, messages) { }
+    }
+}
