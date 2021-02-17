947b1489b7424f9797e97711a97bc8566303a702
Zhanho Shing <zhanhoshing@icloud.com>
2021-02-17 20:00:22 +0800

    feat: add class EntryTypeNotMatchException

---
+++ Settings.Net/Exceptions/EntryTypeNotMatchException.cs
@@ -0,0 +1,34 @@
+﻿#nullable enable
+using System;
+
+namespace Settings.Net.Exceptions {
+    /// <summary>
+    /// Throws if the entry type specified in the JSON does not match the object's type.
+    /// </summary>
+    [Serializable]
+    public class EntryTypeNotMatchException : Exception {
+        private string? _expectedType, _receivedType;
+
+        public EntryTypeNotMatchException() { }
+
+        public EntryTypeNotMatchException(string? expectedType, string? receivedType) {
+            _expectedType = expectedType;
+            _receivedType = receivedType;
+        }
+
+        public EntryTypeNotMatchException(string? expectedType, string? receivedType, string? messages) :
+            base(messages) {
+            _expectedType = expectedType;
+            _receivedType = receivedType;
+        }
+
+        public override string Message {
+            get {
+                var m = base.Message;
+                if (!string.IsNullOrEmpty(_expectedType) && !string.IsNullOrEmpty(_receivedType))
+                    m += $" Expected '{_expectedType}', got '{_receivedType}'";
+                return m;
+            }
+        }
+    }
+}
