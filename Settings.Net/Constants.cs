9194e0da4b003ade9986ee13d3fefb1ff4da4d0f
Zhanho Shing <zhanhoshing@icloud.com>
2021-02-17 18:21:35 +0800

    feat: add Constants class This class contains all constants of the project

---
+++ Settings.Net/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Settings.Net {
+    /// <summary> All constant values of Settings.Net. </summary>
+    [ExcludeFromCodeCoverage]
+    internal static class Constants {
+        /// <summary> Flags indicating the entry's type. </summary>
+        internal static class EntryTypeFlags {
+            internal static string StringEntry => "Settings.StringEntry";
+            internal static string IntEntry => "Settings.IntEntry";
+            internal static string BoolEntry => "Settings.BoolEntry";
+            internal static string EntryGroup => "Settings.EntryGroup";
+        }
+    }
+}
