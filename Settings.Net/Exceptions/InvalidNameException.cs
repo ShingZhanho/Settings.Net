b4d717bb3ac1b4e336c5481578b7025367f39df1
Zhanho Shing <zhanhoshing@icloud.com>
2021-02-17 16:06:24 +0800

    feat: added InvalidNameException

--- Settings.Net/Exceptions/InvalidNameException.cs
+++ Settings.Net/Exceptions/InvalidNameException.cs
@@ -1,7 +1,30 @@
-﻿using System;
+﻿#nullable enable
+using System;
+using System.IO;
 
 namespace Settings.Net.Exceptions {
+    [Serializable]
     public class InvalidNameException : Exception {
+
+        private string _invalidName;
         
+        public InvalidNameException(){ }
+
+        public InvalidNameException(string invalidName) {
+            _invalidName = invalidName;
+        }
+
+        public InvalidNameException(string? invalidName, string? messages) : base(messages){
+            _invalidName = invalidName;
+        }
+
+        public override string Message {
+            get {
+                var s = base.Message;
+                if (!string.IsNullOrEmpty(_invalidName))
+                    s += $" Name \"{_invalidName}\" is invalid.";
+                return s;
+            }
+        }
     }
 }
