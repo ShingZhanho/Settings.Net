678c2185731aa2cdcd9f7626b4b6d68ef0baa811
Zhanho Shing <zhanhoshing@icloud.com>
2021-02-26 23:03:43 +0800

    feat: added multiple changes. added IEntry and IEntry<TValue> interfaces modified SettingEntry and SettingEntry<TValue> classes

---
+++ Settings.Net/IEntry.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+
+namespace Settings.Net {
+    public interface IEntry {
+        /// <summary>
+        /// The ID of the entry. IDs shall not contain any illegal characters.
+        /// </summary>
+        string Id { get; }
+        EntryType Type { get; }
+        IEntry? Parent { get; }
+    }
+    
+    public interface IEntry<out TValue> : IEntry {
+        TValue Value { get; }
+    }
+    
+    public enum EntryType {
+        String,
+        Int,
+        Bool,
+        Group
+    }
+}
--- Settings.Net/SettingEntry.cs
+++ Settings.Net/SettingEntry.cs
@@ -5,40 +5,72 @@ using System.Linq;
 using Settings.Net.Exceptions;
 
 namespace Settings.Net {
-    /// <summary>Represents an entry.</summary>
-    public class SettingEntry<T> {
-        public SettingEntry(string id, T? value, string description) {
+    /// <summary>
+    /// Represents a group of entries.
+    /// </summary>
+    public class SettingEntry : IEntry<List<IEntry>> {
+        /// <summary>
+        /// Initializes a new root of settings.
+        /// </summary>
+        /// <param name="id">The ID of the root.</param>
+        /// <param name="childrenItems">A list of children items which belongs this root.</param>
+        public SettingEntry(string id, List<IEntry> childrenItems) {
+            Id = id;
+            Value = childrenItems;
+            IsRoot = true;
+            Type = EntryType.Group;
+        }
+
+        public string Id { get; }
+        public EntryType Type { get; }
+        public IEntry? Parent { get; internal set; } = null;
+        public List<IEntry> Value { get; }
+        public bool IsRoot { get; }
+
+        public IEntry this[string id] {
+            get {
+                foreach (var item in Value.Where(item => item.Id == id))
+                    return item;
+                throw new ArgumentOutOfRangeException(nameof(id), "Specified ID could not be found.");
+            }
+        }
+    }
+    
+    /// <summary>Represents an entry with value.</summary>
+    public class SettingEntry<TValue> : IEntry<TValue> {
+        public SettingEntry(string id, TValue? value) {
             // Check for accepted types
-            if (!new[] {typeof(string), typeof(int)}.Contains(typeof(T)))
-                throw new ArgumentOutOfRangeException(nameof(T), "This type is not accepted." +
+            if (!new[] {typeof(string), typeof(int), typeof(bool)}.Contains(typeof(TValue)))
+                throw new ArgumentOutOfRangeException(nameof(TValue), "This type is not accepted." +
                                                                  " Accepts only string, int and bool");
             // Check for illegal chars in ID
             if (!IdIsValid(id))
                 throw new InvalidNameException(nameof(id), "IDs should not contain chars: " +
                                                            $"'{GetInvalidIdCharsInString(id)}'");
-            // Assign TypeOfEntry
+            // Assign Type property
             var enumDict = new Dictionary<Type, EntryType>();
-            enumDict.Add(typeof(string), EntryType.StringType);
-            enumDict.Add(typeof(int), EntryType.IntType);
-            enumDict.Add(typeof(bool), EntryType.BoolType);
-            TypeOfEntry = enumDict[typeof(T)];
+            enumDict.Add(typeof(string), EntryType.String);
+            enumDict.Add(typeof(int), EntryType.Int);
+            enumDict.Add(typeof(bool), EntryType.Bool);
+            Type = enumDict[typeof(TValue)];
             // Assign properties
-            ID = id;
-            Description = description;
+            Id = id;
             Value = value;
         }
         
         /// <summary> The ID of the entry. The ID is unique under an entry group. </summary>
-        public string ID { get; }
+        public string Id { get; }
 
         /// <summary> A descriptive line of this entry. Description is optional. </summary>
-        public string Description { get; set; }
+        public string? Description { get; set; }
         
         /// <summary>The value of this entry.</summary>
-        public T? Value { get; internal set; }
+        public TValue? Value { get; }
         
         /// <summary>Gets the type of this entry.</summary>
-        public EntryType TypeOfEntry { get; }
+        public EntryType Type { get; }
+
+        public IEntry Parent { get; }
 
         /// <summary>Gets an array of invalid ID characters.</summary>
         public static char[] InvalidIdChars { get; } = {'.', ' '};
@@ -52,10 +84,5 @@ namespace Settings.Net {
         /// <returns>List of invalid ID chars.</returns>
         public static char[] GetInvalidIdCharsInString(string str) => str.Where(character => InvalidIdChars.Contains(character)).ToArray();
 
-        public enum EntryType {
-            StringType,
-            IntType,
-            BoolType
-        }
     }
 }
