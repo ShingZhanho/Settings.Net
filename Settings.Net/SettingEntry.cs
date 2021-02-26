b11cc5df7e604e16312598f7407d1f38ef16dd08
Zhanho Shing <zhanhoshing@icloud.com>
2021-02-26 18:14:52 +0800

    feat: added constructor

--- Settings.Net/SettingEntry.cs
+++ Settings.Net/SettingEntry.cs
@@ -1,18 +1,44 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using Settings.Net.Exceptions;
 
 namespace Settings.Net {
-    /// <summary> A base class for settings entry. This class is abstract and shall not be used in your code. </summary>
-    [ExcludeFromCodeCoverage]
-    public abstract class SettingEntry<T> {
+    /// <summary>Represents an entry.</summary>
+    public class SettingEntry<T> {
+        public SettingEntry(string id, T? value, string description) {
+            // Check for accepted types
+            if (!new[] {typeof(string), typeof(int)}.Contains(typeof(T)))
+                throw new ArgumentOutOfRangeException(nameof(T), "This type is not accepted." +
+                                                                 " Accepts only string, int and bool");
+            // Check for illegal chars in ID
+            if (!IdIsValid(id))
+                throw new InvalidNameException(nameof(id), "IDs should not contain chars: " +
+                                                           $"'{GetInvalidIdCharsInString(id)}'");
+            // Assign TypeOfEntry
+            var enumDict = new Dictionary<Type, EntryType>();
+            enumDict.Add(typeof(string), EntryType.StringType);
+            enumDict.Add(typeof(int), EntryType.IntType);
+            enumDict.Add(typeof(bool), EntryType.BoolType);
+            TypeOfEntry = enumDict[typeof(T)];
+            // Assign properties
+            ID = id;
+            Description = description;
+            Value = value;
+        }
+        
         /// <summary> The ID of the entry. The ID is unique under an entry group. </summary>
-        public abstract string ID { get; }
+        public string ID { get; }
 
         /// <summary> A descriptive line of this entry. Description is optional. </summary>
-        public abstract string Description { get; set; }
+        public string Description { get; set; }
         
         /// <summary>The value of this entry.</summary>
-        public abstract T Value { get; internal set; }
+        public T? Value { get; internal set; }
+        
+        /// <summary>Gets the type of this entry.</summary>
+        public EntryType TypeOfEntry { get; }
 
         /// <summary>Gets an array of invalid ID characters.</summary>
         public static char[] InvalidIdChars { get; } = {'.', ' '};
@@ -25,5 +51,11 @@ namespace Settings.Net {
         /// <summary> Gets the list of chars that are illegal to use for an ID name. </summary>
         /// <returns>List of invalid ID chars.</returns>
         public static char[] GetInvalidIdCharsInString(string str) => str.Where(character => InvalidIdChars.Contains(character)).ToArray();
+
+        public enum EntryType {
+            StringType,
+            IntType,
+            BoolType
+        }
     }
 }
