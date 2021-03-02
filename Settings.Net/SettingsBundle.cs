50f871d411e5a3f49208aa3b422944a77471d0b2
Zhanho Shing <zhanhoshing@icloud.com>
2021-03-02 14:20:12 +0800

    feat: added JSON constructor for SettingsBundle class

--- Settings.Net/SettingsBundle.cs
+++ Settings.Net/SettingsBundle.cs
@@ -1,4 +1,11 @@
-﻿#nullable enable
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Settings.Net.Exceptions;
+
+#nullable enable
 
 namespace Settings.Net
 {
@@ -7,10 +14,7 @@ namespace Settings.Net
         /// <summary>
         /// Initializes a new SettingsBundle with no values.
         /// </summary>
-        public SettingsBundle()
-        {
-            
-        }
+        public SettingsBundle() { }
         
         /// <summary>
         /// Initializes a SettingsBundle from an existing .sbd file.
@@ -18,7 +22,47 @@ namespace Settings.Net
         /// <param name="filePath">The path to the .sbd file.</param>
         public SettingsBundle(string filePath)
         {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("The specified file could not be found.", filePath);
+            string jsonString;
+            try
+            {
+                jsonString = File.ReadAllText(filePath);
+                _ = JObject.Parse(jsonString); // Validate JSON
+            }
+            catch (JsonReaderException e)
+            { // Invalid Json
+                throw new JsonReaderException("Not a valid JSON data.", e);
+            }
+            catch (Exception e)
+            { // Error while reading JSON.
+                throw new IOException("Error while trying to read the file.", e);
+            }
+            PrivateConstructor(jsonString);
+        }
+
+        public SettingsBundle(JToken jToken) => PrivateConstructor(jToken);
+
+        public string? Description { get; set; }
+        public List<SettingsGroup> Roots { get; private set; } = new ();
+
+        private void PrivateConstructor(JToken jToken)
+        {
+            // Construct a new object with JToken
+            InternalEnsureJsonState(jToken);
+            Description = jToken["metadata"]!["desc"]!.ToString();
             
+        }
+
+        private static void InternalEnsureJsonState(JToken jToken)
+        {
+            // Check json data for errors
+            // Check "metadata" key
+            if (!((JObject) jToken).ContainsKey("metadata"))
+                throw new InvalidEntryTokenException("metadata", "The file does not have the essential key 'metadata'");
+            // Check "data" key
+            if (!((JObject) jToken).ContainsKey("data"))
+                throw new InvalidEntryTokenException("data", "The file does not have the essential key 'data'");
         }
     }
 }
