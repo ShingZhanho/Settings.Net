fdd3f921b2a34086cebbd8fe510e8b08607b19bb
Zhanho Shing <zhanhoshing@icloud.com>
2021-03-02 09:29:32 +0800

    feat: added SettingsBundle class

---
+++ Settings.Net/SettingsBundle.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+
+namespace Settings.Net
+{
+    public class SettingsBundle
+    {
+        /// <summary>
+        /// Initializes a new SettingsBundle with no values.
+        /// </summary>
+        public SettingsBundle()
+        {
+            
+        }
+        
+        /// <summary>
+        /// Initializes a SettingsBundle from an existing .sbd file.
+        /// </summary>
+        /// <param name="filePath">The path to the .sbd file.</param>
+        public SettingsBundle(string filePath)
+        {
+            
+        }
+    }
+}
