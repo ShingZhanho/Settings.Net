9ddec42e48a71d0b7f107bdf3e2d5c9260e8e482
Zhanho Shing <zhanhoshing@icloud.com>
2021-03-04 21:48:48 +0800

    test: added a test for SettingsBundle class

---
+++ Settings.Net.Tests/SettingsBundleTests.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+using System;
+using NUnit.Framework;
+
+namespace Settings.Net.Tests
+{
+    public class SettingsBundleTests
+    {
+        [Test,
+        Description("Construct a new SettingsBundle, no exceptions should be thrown.")]
+        public void Ctor_NewBundle_Successful()
+        {
+            // Arrange
+            static SettingsBundle Action(object? _) => new ();
+
+            // Act
+            var exception = TestUtils.GetExceptionFromFunction(Action, null, out var result);
+            
+            // Assert
+            Assert.That(exception, Is.Null);
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Roots.Count, Is.EqualTo(0));
+        }
+    }
+}
