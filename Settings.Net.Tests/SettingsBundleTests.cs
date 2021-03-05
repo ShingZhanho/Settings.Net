332a8fda95bfd476b611169e93a55e58820cd7ea
Zhanho Shing <zhanhoshing@icloud.com>
2021-03-05 12:07:03 +0800

    test: simplified tests removed redundant TestUtils class

--- Settings.Net.Tests/SettingsBundleTests.cs
+++ Settings.Net.Tests/SettingsBundleTests.cs
@@ -14,14 +14,10 @@ namespace Settings.Net.Tests
         Description("Construct a new SettingsBundle, no exceptions should be thrown.")]
         public void Ctor_NewBundle_Successful()
         {
-            // Arrange
-            static SettingsBundle Action(object? _) => new ();
-
             // Act
-            var exception = TestUtils.GetExceptionFromFunction(Action, null, out var result);
+            var result = new SettingsBundle();
             
             // Assert
-            Assert.That(exception, Is.Null);
             Assert.That(result, Is.Not.Null);
             Assert.That(result.Roots.Count, Is.EqualTo(0));
         }
@@ -34,21 +30,14 @@ namespace Settings.Net.Tests
             var randomizer = new Randomizer();
             var id = randomizer.GetString(randomizer.Next(1,15));
             var desc = randomizer.GetString(randomizer.Next(5, 30));
-            static SettingsBundle Action(object? parameter)
-            {
-                var list = (List<string>) parameter!;
-                var bundle = new SettingsBundle();
-                bundle.AddNewRoot(list[0], null, list[1]);
-                return bundle;
-            }
+            var bundle = new SettingsBundle();
 
             // Act
-            var exception = TestUtils.GetExceptionFromFunction(Action, new List<string> {id, desc}, out var result);
+            bundle.AddNewRoot(id, null, desc);
             
             // Assert
-            Assert.That(exception, Is.Null);
-            Assert.That(result, Is.Not.Null);
-            Assert.That(result.ContainsKey(id), Is.True);
+            Assert.That(bundle, Is.Not.Null);
+            Assert.That(bundle.ContainsKey(id), Is.True);
         }
     }
 }
