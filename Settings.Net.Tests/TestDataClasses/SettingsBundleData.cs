12ea8e1829ddfb69b48ed3c40f598e008ee7993e
Zhanho Shing <zhanhoshing@icloud.com>
2021-03-05 11:56:34 +0800

    feat: rearranged file structure

---
+++ Settings.Net.Tests/TestDataClasses/SettingsBundleData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Settings.Net.Tests
+{
+    public partial class TestData
+    {
+        private static string CurrentDir => Environment.CurrentDirectory;
+        private static string FolderPath => Path.Combine(CurrentDir, "TestData");
+        
+        public static class SettingsBundleData
+        {
+            private static string FolderPath =>
+                Path.Combine(TestData.FolderPath, "SettingsBundle");
+
+            public static string NormalJsonFilePath => Path.Combine(FolderPath, "NormalSettingsFile.sj");
+        }
+    }
+}
